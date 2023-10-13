d9d0d175e557132fc649ca266139c86afb181092
Sakshi Shailendra Gole <122728264+Sakshigole2003@users.noreply.github.com>
2023-10-13 11:46:01 +0530

    Add files via upload

---
+++ images/mission.js
@@ -0,0 +1,24 @@
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
